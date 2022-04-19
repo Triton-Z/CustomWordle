9d288fbb11981ef367c76116b3ebe2e2d6901c5c
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-04-19 11:30:31 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ function button (){
   } else {
     for (var xLevel = 1; xLevel<6; xLevel++)
     {
-      document.getElementById("x"+xLevel+"y"+yLevel).innerHTML = word.substring(xLevel-1, xLevel);
+      document.getElementById("x"+xLevel+"y"+yLevel).innerHTML = word.substring(xLevel, xLevel+1);
       i-=1;
     }
   }
