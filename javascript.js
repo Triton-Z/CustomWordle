541cdb3d3c43a148ed31bc2d650e92b6ab1faf4e
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-04-19 09:27:28 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-
+var buttonText = document.getElementById("userButton");
+var word = "";
+var yLevel = 1;
+function button (){
+  if (document.getElementById("userButton").value == "Enter Word")
+  {
+    if (word.length==5)
+    {  
+      word = document.getElementById("userText").value;
+      buttonText.innerHTML = "Guess Word";
+    } 
+  } else {
+    for (var xLevel = 1; x<6; x++;)
+    {
+      document.getElementById(xLevel+"x"+yLevel+"y").innerHTML = word.substring(x-1, x);
+      
+    }
+  }
+}
