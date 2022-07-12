b9e6471a2aad361f9a80fbc8ccb84143759fa398
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-07-11 21:16:47 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ var wordle = "";
 var y = 6;
 function createWord (){
   wordle = document.getElementById("userText").value;
-  for (var x = 0; x<6; x++)
+  for (var x = 1; x<6; x++)
   {
    document.getElementById(`x${x}y${y}`).innerHTML = wordle[x];
   }
